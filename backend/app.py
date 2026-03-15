1f92699b3f8bd1d8b7ecc605b2f4f4f1624ad90b
PuliAmrutha <puliamruthareddy@gmail.com>
2026-03-15 21:50:50 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,24 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from predict import predict_news
+import os
 
 app = Flask(__name__)
 CORS(app)
 
+# Home route
 @app.route("/")
 def home():
     return "Fake News Detection API Running"
 
+# Prediction route
 @app.route("/predict", methods=["POST"])
 def predict():
-
+    
     data = request.get_json()
+
+    if "news" not in data:
+        return jsonify({"error": "No news text provided"}), 400
 
     text = data["news"]
 
@@ -23,5 +29,8 @@ def predict():
         "confidence": confidence
     })
 
+
+# Run the app (important for Railway)
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
